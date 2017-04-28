ba287153d583aa6160f2deb9d22f0805ff33f4ed
bkolicoski <bkolicoski@gmail.com>
2017-04-28 23:23:31 +0200

    Update IronManArcReactorFlicker.ino
    Added brightness pinging and implemented boot sequence

--- IronManArcReactorFlicker.ino
+++ IronManArcReactorFlicker.ino
@@ -11,24 +11,38 @@ int ledPin2 = 1;
 int minBrightness = 50;
 int maxBrightness = 200;
 
+unsigned long lastPing;
+unsigned long currentTime;
+
 void setup() {
   pinMode(ledPin1, OUTPUT);
   pinMode(ledPin2, OUTPUT);
+
+  bootSequence();
+  lastPing = millis();
 }
 
 void loop() {
   analogWrite(ledPin1, random(minBrightness, maxBrightness));
   analogWrite(ledPin2, random(minBrightness, maxBrightness));
   delay(random(100));
+  currentTime = millis();
+  //check for ping every 3 sec
+  if(currentTime >= lastPing + 3000){
+    lastPing = currentTime;
+    analogWrite(ledPin1, 255);
+    analogWrite(ledPin2, 255);
+    delay(200);
+  }
 }
 
 void bootSequence(){
   //ramp up to full brightness 
-  for (int i=0; i<=255; i+=5)
+  for (int i=0; i<=255; i+=10)
   {
     analogWrite(ledPin1, i);
     analogWrite(ledPin2, i);
-    delay(10);
+    delay(50);
   }
 }
 
